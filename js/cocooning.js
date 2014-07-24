d4849fa46afcafac2c8e8c2d139f84005f51557f
milo999 <mileslevoguer@hotmail.co.uk>
2014-07-24 15:30:04 +0100

    fgdfg

---
+++ js/cocooning.js
@@ -0,0 +1,9 @@
+var promise = Kinvey.init({
+            appKey    : "kid_VeqRMNxJ8i",
+            appSecret : "6832159125124fdfb3cf18f7a6439f8b"
+        });
+        promise.then(function(activeUser) {
+            // Your code here
+        });
+        
+        undefined
